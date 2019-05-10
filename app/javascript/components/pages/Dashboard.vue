17d62f08ea3f751eae376e06b15a2c47cbf928d0
Vincent Chu <vincentchu5407@gmail.com>
2019-05-09 20:51:46 -0700

    Added comments on what to keep

--- app/javascript/components/pages/Dashboard.vue
+++ app/javascript/components/pages/Dashboard.vue
@@ -1,53 +1,37 @@
 
 <template>
   <div>
+    <!-- KEEP THIS CODE BELOW -->
     <button v-on:click="showApartment">Click Apartment Here</button>
     <Apartment v-if="hide"/>
+    <!-- KEEP THIS CODE ABOVE -->
+
     <b-carousel
-      id="carousel-1"
+      id="carousel-fade"
       v-model="slide"
       :interval="4000"
       controls
       indicators
       background="#ababab"
-      img-width="1024"
-      img-height="480"
+      img-width="550"
+      img-height="450"
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
-
       <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+      <b-carousel-slide img-src="/DOM.JPG">
         <h1>Hello world!</h1>
       </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <img
-          slot="img"
-          class="d-block img-fluid w-100"
-          width="400"
-          height="200"
-          src="CSE110Logo.png"
-          alt="image slot"
-        >
+      <b-carousel-slide img-src="/EX.JPG">
+        <h1>Hello world!</h1>
       </b-carousel-slide>
     </b-carousel>
   </div>
 </template>
 
 <script>
+// KEEP ALL HERE
 import Apartment from "./ApartmentMate.vue";
 export default {
   name: "Dashboard",
