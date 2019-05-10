c7a5d6fa1ca9a2b1016b8ad198e667ecfac1e885
Andrew Tang <andrewtang9@gmail.com>
2019-05-09 17:26:40 -0700

    added Notifications

--- app/javascript/router.js
+++ app/javascript/router.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router';
 
 import Login from './components/pages/Login'
 import Two from './components/pages/Two'
+import Notifications from './components/pages/Notifications'
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/Login', component: Login },
   { path: '/two', component: Two },
+  { path: '/Notifications', component: Notifications},
   { path: '*', redirect: '/Login' }
 ];
 
