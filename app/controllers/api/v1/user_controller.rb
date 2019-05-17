62bd0a9520a8b9d38676ecc97ce3ab6828c07a2f
Kevin Tang <kevin_m_tang@yahoo.com>
2019-05-16 17:31:34 -0700

    update user_controller for code review

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,10 +2,10 @@
 
 # localhost:3000/api/v1/user
 class Api::V1::UserController < ApplicationController
-  before_action :authenticated?, only: [:delete]
+  before_action :authenticated?, only: [:delete, :update_profile]
 
-  # Add new user
-  def create
+  # Register new user
+  def register
     user = User.find_by username: params[:account_name]
     return render plain: 'User with this username already exists', status: :bad_request unless user.nil?
 
@@ -19,29 +19,41 @@ class Api::V1::UserController < ApplicationController
     )
     return render :json => {:errors => user.errors.full_messages}, status: :bad_request unless user.valid?
 
-    render plain: 'User creation successful'
+    render plain: 'User creation successful', status: :ok
   end
 
-  # Delete (requires auth)
   def delete
-    User.find_by_username(params[:username]).destroy
+    user = User.find_by_username(params[:auth][:username])
+    user.destroy
     head :ok
   end
 
   def find_by_reset_token
     user = User.find_by_reset_token(params[:reset_token])
-    render json: 'invalid token', status: :unauthorized if user.nil?
+    render plain: 'invalid token', status: :unauthorized if user.nil?
 
     render json: {username: user.username}
   end
 
-  # Reset password of user
+  # Reset password of user using reset token
   def reset_password
     user = User.find_by_reset_token(params[:reset_token])
-    render json: 'invalid token', status: :unauthorized if user.nil?
+    render plain: 'invalid token', status: :unauthorized if user.nil?
+    user.update_password(params[:new_password])
+    render plain: 'Password successfully updated', status: :ok
+  end
 
-    user.update_password(params[:password])
+  # Update first name, last name, password
+  def update_profile
+    user = User.find_by_username(params[:auth][:username])
+    render json: 'invalid user id', status: :unauthorized if user.nil?
 
-    head :ok
+    if user.update({:first_name => params[:first_name],
+                    :last_name => params[:last_name],
+                    :password => params[:password]}.reject{|_,v| v.blank?})
+      render plain: 'Profile successfully updated', status: :ok
+    else
+      render :json => {:errors => user.errors.full_messages}, status: :bad_request
+    end
   end
 end
