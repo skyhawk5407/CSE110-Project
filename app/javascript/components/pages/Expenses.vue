2167d1e87d2c22125ddd4f490fb292a9e1860dba
AndrewEToh <aetoh@ucsd.edu>
2019-05-31 16:14:19 -0700

    Fixed removeExp() bug | Current issue with multiple module dependencies

--- app/javascript/components/pages/Expenses.vue
+++ app/javascript/components/pages/Expenses.vue
@@ -2,26 +2,7 @@
   <div>
     <b-jumbotron>
       <template slot="header">Expenses</template>
-
-      <!-- Add Expense Modal and Button -->
-      <b-button v-b-modal.modal-add variant="primary">Add Expense</b-button>
-      <b-modal id="modal-add" hide-footer title="Add Expense">
-        <!-- Expense Fields -->
-        <label>Name of Expense:</label>
-        <b-form-input v-model="expense_title" placeholder="Name"></b-form-input>
-        <label>Amount:</label>
-        <b-form-input v-model="expense_amount" placeholder="$0.00"></b-form-input>
-        <label>Description:</label>
-        <b-form-textarea v-model="expense_description" rows="3" placeholder="Description"></b-form-textarea>
-        <label>Payer:</label>
-        <b-form-input v-model="expense_payer_id" placeholder="Payer"></b-form-input>
-        <!-- Options -->
-        <b-button class="mt-2" variant="info" @click="addExpense">Add Expense</b-button>
-        <b-button class="mt-2" variant="danger" @click="$bvModal.hide('modal-add')">Cancel</b-button>
-      </b-modal>
-
       <label>Expenses Owed</label>
-
       <b-table show-empty stacked="md" :items="expense_entries" :fields="fields">
         <!-- Props to b-table to add later TODO -->
         <!-- :filter="filter"
@@ -36,7 +17,6 @@
         <template slot="Issuer" slot-scope="col">{{ col.value }}</template>
         <template slot="Payers" slot-scope="col">{{ col.value }}</template>
         <template slot="Description" slot-scope="col">{{ col.value }}</template>
-        <template slot="Paid" slot-scope="col">{{ col.value }}</template>
         <!-- Actions -->
         <template slot="Actions" slot-scope="row">
           <!-- Pay Expense Modal and Button -->
@@ -63,12 +43,10 @@
               @click="$bvModal.hide('modal-remove')"
             >No, I am not sure.</b-button>
             <b-button class="mt-2" variant="danger" @click="removeExp(row.item)">Yes, I am sure.</b-button>
-          </b-modal>-->
+          </b-modal> -->
         </template>
       </b-table>
-
       <label>Expenses Issued</label>
-
       <b-table show-empty stacked="md" :items="issued_expense_entries" :fields="fields">
         <!-- Props to b-table to add later TODO -->
         <!-- :filter="filter"
@@ -83,7 +61,6 @@
         <template slot="Issuer" slot-scope="col">{{ col.value }}</template>
         <template slot="Payers" slot-scope="col">{{ col.value }}</template>
         <template slot="Description" slot-scope="col">{{ col.value }}</template>
-        <template slot="Paid" slot-scope="col">{{ col.value }}</template>
         <!-- Actions -->
         <template slot="Actions" slot-scope="row">
           <!-- Pay Expense Indicator -->
@@ -94,8 +71,8 @@
             </p>
             <b-button class="mt-2" variant="info" @click="$bvModal.hide('modal-pay')">Pay expense</b-button>
             <b-button class="mt-2" variant="danger" @click="$bvModal.hide('modal-pay')">Cancel</b-button>
-          </b-modal>-->
-          <!-- Remove Expense Modal and Button-->
+          </b-modal> -->
+          <!-- Remove Expense Modal     and Button -->
           <b-button v-b-modal.modal-remove variant="danger" class="btn-sm">Remove</b-button>
           <b-modal id="modal-remove" hide-footer title="Remove Expense" no-stacking>
             <p>
@@ -113,9 +90,26 @@
           </b-modal>
         </template>
       </b-table>
+      <!-- Add Expense Modal and Button -->
+      <b-button v-b-modal.modal-add variant="primary">Add Expense</b-button>
+      <b-modal id="modal-add" hide-footer title="Add Expense">
+        <!-- Expense Fields -->
+        <label>Name of Expense:</label>
+        <b-form-input v-model="expense_title" placeholder="Name"></b-form-input>
+        <label>Amount:</label>
+        <b-form-input v-model="expense_amount" placeholder="$0.00"></b-form-input>
+        <label>Description:</label>
+        <b-form-textarea v-model="expense_description" rows="3" placeholder="Description"></b-form-textarea>
+        <label>Payer:</label>
+        <b-form-input v-model="expense_payer_id" placeholder="Payer"></b-form-input>
+        <!-- Options -->
+        <b-button class="mt-2" variant="info" @click="addExpense">Add Expense</b-button>
+        <b-button class="mt-2" variant="danger" @click="$bvModal.hide('modal-add')">Cancel</b-button>
+      </b-modal>
     </b-jumbotron>
   </div>
 </template>
+
 <script>
 import api from "../../api.js";
 import moment from "moment";
@@ -181,19 +175,19 @@ export default {
       var dollars = int(value / 100);
       var cents = value - dollar * 100;
       currency = "$" + String(dollars) + "." + String(cents);
-      console.log(currency);
       return currency;
     },
     async removeExp(row) {
+        console.log(row);
       var amount = 1;
-      var index = this.expense_entries.indexOf(row);
-      var expense_id = this.expense_entries[index].Id;
+      var index = this.issued_expense_entries.indexOf(row);
+      var expense_id = this.issued_expense_entries[index].Id;
       let response = await api.expenses.delete(
         expense_id,
         this.current_user_email,
         this.current_user_password
       );
-      this.expense_entries.splice(index, amount);
+      this.issued_expense_entries.splice(index, amount);
       this.$bvModal.hide("modal-remove");
     },
     // This should split the costs and submit multiple expenses to the backend TODO FIX THIS TO UPDATED EXPENSE ENTRIES
@@ -261,8 +255,7 @@ export default {
           Amount: lastTransactionEntry.amount,
           Issuer: lastTransactionEntry.issuer.display_name,
           Payers: lastTransactionEntry.payer.display_name,
-          Description: lastTransactionEntry.description,
-          Paid: lastTransactionEntry.paid
+          Description: lastTransactionEntry.description
         });
       } catch (err) {
         // Error handling
@@ -295,7 +288,7 @@ export default {
       var i = 0;
       for (i in transactions) {
         var entry = transactions[i];
-        console.log(entry.issuer.id);
+        // console.log(entry.id);
         // add to table
         var load = {
           Id: entry.id,
@@ -304,8 +297,7 @@ export default {
           Amount: entry.amount,
           Issuer: entry.issuer.display_name,
           Payers: entry.payer.display_name,
-          Description: entry.description,
-          Paid: entry.paid
+          Description: entry.description
         };
         if (entry.issuer.id == this.expense_issuer_id) {
           this.issued_expense_entries.push(load);
