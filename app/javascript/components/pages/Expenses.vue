3f39fddc54b87b74e620a66efcd9dd3bb2592b47
AndrewEToh <aetoh@ucsd.edu>
2019-05-31 15:18:18 -0700

    filtered expenses specific to user | filtered expenses between issued and owed | disabled 'remove' button for owed expenses | disabled 'pay' button for issued expenses | currently attempting to implement status indicator | currently attempting to implement currency converter

--- app/javascript/components/pages/Expenses.vue
+++ app/javascript/components/pages/Expenses.vue
@@ -2,6 +2,23 @@
   <div>
     <b-jumbotron>
       <template slot="header">Expenses</template>
+      <!-- Add Expense Modal and Button -->
+      <b-button v-b-modal.modal-add variant="primary">Add Expense</b-button>
+      <b-modal id="modal-add" hide-footer title="Add Expense">
+        <!-- Expense Fields -->
+        <label>Name of Expense:</label>
+        <b-form-input v-model="expense_title" placeholder="Name"></b-form-input>
+        <label>Amount:</label>
+        <b-form-input v-model="expense_amount" placeholder="$0.00"></b-form-input>
+        <label>Description:</label>
+        <b-form-textarea v-model="expense_description" rows="3" placeholder="Description"></b-form-textarea>
+        <label>Payer:</label>
+        <b-form-input v-model="expense_payer_id" placeholder="Payer"></b-form-input>
+        <!-- Options -->
+        <b-button class="mt-2" variant="info" @click="addExpense">Add Expense</b-button>
+        <b-button class="mt-2" variant="danger" @click="$bvModal.hide('modal-add')">Cancel</b-button>
+      </b-modal>
+      <label>Expenses Owed</label>
       <b-table show-empty stacked="md" :items="expense_entries" :fields="fields">
         <!-- Props to b-table to add later TODO -->
         <!-- :filter="filter"
@@ -16,10 +33,11 @@
         <template slot="Issuer" slot-scope="col">{{ col.value }}</template>
         <template slot="Payers" slot-scope="col">{{ col.value }}</template>
         <template slot="Description" slot-scope="col">{{ col.value }}</template>
+        <template slot="Paid" slot-scope="col">{{ col.value }}</template>
         <!-- Actions -->
         <template slot="Actions" slot-scope="row">
           <!-- Pay Expense Modal and Button -->
-          <b-button v-b-modal.modal-pay variant="primary" class="btn-sm">Pay Me</b-button>
+          <b-button v-b-modal.modal-pay variant="primary" class="btn-sm">Pay</b-button>
           <b-modal id="modal-pay" hide-footer title="Pay Expense" no-stacking>
             <p>
               <b>Do you want to pay for this expense?</b>
@@ -45,26 +63,54 @@
           </b-modal>
         </template>
       </b-table>
-      <!-- Add Expense Modal and Button -->
-      <b-button v-b-modal.modal-add variant="primary">Add Expense</b-button>
-      <b-modal id="modal-add" hide-footer title="Add Expense">
-        <!-- Expense Fields -->
-        <label>Name of Expense:</label>
-        <b-form-input v-model="expense_title"></b-form-input>
-        <label>Amount:</label>
-        <b-form-input v-model="expense_amount"></b-form-input>
-        <label>Description:</label>
-        <b-form-textarea v-model="expense_description" rows="3"></b-form-textarea>
-        <label>Payer:</label>
-        <b-form-input v-model="expense_payer_id"></b-form-input>
-        <!-- Options -->
-        <b-button class="mt-2" variant="info" @click="addExpense">Add Expense</b-button>
-        <b-button class="mt-2" variant="danger" @click="$bvModal.hide('modal-add')">Cancel</b-button>
-      </b-modal>
+      <label>Expenses Issued</label>
+      <b-table show-empty stacked="md" :items="issued_expense_entries" :fields="fields">
+        <!-- Props to b-table to add later TODO -->
+        <!-- :filter="filter"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :sort-direction="sortDirection"
+        @filtered="onFiltered"-->
+        <!-- Data -->
+        <template slot="Date" slot-scope="col">{{ col.value }}</template>
+        <template slot="Expense" slot-scope="col">{{ col.value }}</template>
+        <template slot="Amount" slot-scope="col">{{ col.value }}</template>
+        <template slot="Issuer" slot-scope="col">{{ col.value }}</template>
+        <template slot="Payers" slot-scope="col">{{ col.value }}</template>
+        <template slot="Description" slot-scope="col">{{ col.value }}</template>
+        <template slot="Paid" slot-scope="col">{{ col.value }}</template>
+        <!-- Actions -->
+        <template slot="Actions" slot-scope="row">
+          <!-- Pay Expense Indicator -->
+          <!-- <b-button v-b-modal.modal-pay variant="primary" class="btn-sm">Pay Me</b-button>
+          <b-modal id="modal-pay" hide-footer title="Pay Expense" no-stacking>
+            <p>
+              <b>Do you want to pay for this expense?</b>
+            </p>
+            <b-button class="mt-2" variant="info" @click="$bvModal.hide('modal-pay')">Pay expense</b-button>
+            <b-button class="mt-2" variant="danger" @click="$bvModal.hide('modal-pay')">Cancel</b-button>
+          </b-modal>-->
+          <!-- Remove Expense Modal and Button-->
+          <b-button v-b-modal.modal-remove variant="danger" class="btn-sm">Remove</b-button>
+          <b-modal id="modal-remove" hide-footer title="Remove Expense" no-stacking>
+            <p>
+              <b>Are you SURE you wish to remove this expense?</b>
+              <br>
+              <br>
+              <i>Note: This action can not be undone.</i>
+            </p>
+            <b-button
+              class="mt-2"
+              variant="info"
+              @click="$bvModal.hide('modal-remove')"
+            >No, I am not sure.</b-button>
+            <b-button class="mt-2" variant="danger" @click="removeExp(row.item)">Yes, I am sure.</b-button>
+          </b-modal>
+        </template>
+      </b-table>
     </b-jumbotron>
   </div>
 </template>
-
 <script>
 import api from "../../api.js";
 import moment from "moment";
@@ -88,21 +134,21 @@ export default {
         "Description",
         "Actions"
       ],
-      expense_entries: [],
       apartment_mates: [],
+      expense_entries: [],
+      issued_expense_entries: [],
 
       current_user_email: this.$store.state.username,
       current_user_password: this.$store.state.password,
-      expense_payer_id: undefined,
-      expense_issuer_id: undefined,
+      expense_payer_id: null,
+      expense_issuer_id: null,
       expense_title: undefined,
       expense_description: undefined,
-      expense_amount: undefined
+      expense_amount: null
     };
   },
   created() {
     this.extractMates();
-    this.getAllExpenses();
   },
   methods: {
     async extractMates() {
@@ -122,14 +168,16 @@ export default {
           display_name: apt_mate.display_name
         });
       }
+
+      this.getAllExpenses();
     },
     async convertToCurrency(value) {
-        var currency = "";
-        var dollars = (int) (value / 100);
-        var cents = value - (dollar*100);
-        currency = "$" + String(dollars) + "." + String(cents);
-        console.log(currency);
-        return currency;
+      var currency = "";
+      var dollars = int(value / 100);
+      var cents = value - dollar * 100;
+      currency = "$" + String(dollars) + "." + String(cents);
+      console.log(currency);
+      return currency;
     },
     async removeExp(row) {
       var amount = 1;
@@ -201,14 +249,15 @@ export default {
         );
         console.log(lastTransactionEntry);
         // add to table
-        this.expense_entries.push({
+        this.issued_expense_entries.push({
           Id: lastTransactionEntry.id,
           Date: moment(lastTransactionEntry.created_at).format("MM/DD/YYYY"),
           Expense: lastTransactionEntry.title,
           Amount: lastTransactionEntry.amount,
           Issuer: lastTransactionEntry.issuer.display_name,
           Payers: lastTransactionEntry.payer.display_name,
-          Description: lastTransactionEntry.description
+          Description: lastTransactionEntry.description,
+          Paid: lastTransactionEntry.paid
         });
       } catch (err) {
         // Error handling
@@ -241,16 +290,23 @@ export default {
       var i = 0;
       for (i in transactions) {
         var entry = transactions[i];
+        console.log(entry.issuer.id);
         // add to table
-        this.expense_entries.push({
+        var load = {
           Id: entry.id,
           Date: moment(entry.created_at).format("MM/DD/YYYY"),
           Expense: entry.title,
           Amount: entry.amount,
           Issuer: entry.issuer.display_name,
           Payers: entry.payer.display_name,
-          Description: entry.description
-        });
+          Description: entry.description,
+          Paid: entry.paid
+        };
+        if (entry.issuer.id == this.expense_issuer_id) {
+          this.issued_expense_entries.push(load);
+        } else {
+          this.expense_entries.push(load);
+        }
       }
     }
   }
